5ba15a5652fb8ed42490a106698a40b9c08c3540
Altaf Quadri <altafmquadri@gmail.com>
2020-02-24 16:10:40 -0500

    unstyled color slider

--- src/App.js
+++ src/App.js
@@ -8,10 +8,9 @@ import seedColors from './seedColors'
 class App extends Component {
   state = {}
   render() {
-    console.log(generatePalette(seedColors[4]))
     return (
       <div>
-        <Palette {...seedColors[4]} />
+        <Palette palette={generatePalette(seedColors[4])} />
       </div>
     );
   }
--- src/Palette.js
+++ src/Palette.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
+import Slider, { Range } from 'rc-slider';
 import ColorBox from './ColorBox'
 import './Palette.css'
+import 'rc-slider/assets/index.css';
 
 class Palette extends Component {
-    state = {}
+    state = {
+        level: 500
+    }
+
+    changeLevel = (level) => {
+        this.setState({ level });
+    }
     render() {
-        const colorBoxes = this.props.colors.map(color => (
-            <ColorBox background={color.color} name={color.name} />))
+        const { level } = this.state
+        const { colors } = this.props.palette
+        const colorBoxes = colors[level].map(color => (
+            <ColorBox background={color.hex} name={color.name} />))
+            
         return (
             <div className='Palette'>
+                <Slider defaultValue={level}
+                    min={100}
+                    max={900}
+                    step={100}
+                    onAfterChange={this.changeLevel} />
                 {/* Navbar goes here */}
                 <div className='Palette-colors'>
                     {colorBoxes}
