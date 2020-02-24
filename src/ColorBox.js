e1e7a8c53afc1f6a09aef920a1650ee6b2bdd464
Altaf Quadri <altafmquadri@gmail.com>
2020-02-23 21:17:55 -0500

    ColorBox

---
+++ src/ColorBox.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './ColorBox.css'
+
+class ColorBox extends Component {
+    state = {}
+    render() {
+        console.log(this.props)
+        return (
+            <div style={{ background: this.props.background }} className='ColorBox'>
+                <span>{this.props.name}</span>
+                <span>More</span>
+            </div>
+        )
+    }
+}
+
+export default ColorBox;
